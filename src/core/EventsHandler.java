580af1aaf811c42950c65a07b974c6b82557fa3e
BrunooMPS <61566542+BrunooMPS@users.noreply.github.com>
2021-12-28 03:52:11 +0000

    Lista eventos com por ID´s

--- src/core/EventsHandler.java
+++ src/core/EventsHandler.java
@@ -1,13 +1,14 @@
 package core;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class EventsHandler {
-
 
 	public void createFile() {
 
@@ -24,34 +25,55 @@ public class EventsHandler {
 		}
 	}
 
-	public void writeToFile(String text) {
+	public void writeToFile(String text) throws IOException {
 
+			int lastIndex = numberOfEvents();
+			
 		try {
 			FileWriter events = new FileWriter("events/events.txt",true);
-			events.write(text+"\n");
-			
+			events.write(lastIndex+" "+text+"\n");
 			events.close();
-		
 			System.out.println("Successfully wrote to the file.");
+			//setline_counter(line_counter++);
 		} catch (IOException e) {
 			System.err.println("An error occurred.");
 			e.printStackTrace();
 		}
+		lastIndex = 0;
 	}
-	
-	public void readEvents() {
-		
+
+	public void listEvents() {
+
 		try {
 			File eventsToRead = new File("events/events.txt");
 			Scanner reader = new java.util.Scanner(eventsToRead);
 			while(reader.hasNextLine()) {
 				System.out.println("Read from file "+reader.nextLine());
 			}
-			
+
 			reader.close();
 		}catch (FileNotFoundException e) {
 			System.err.println("An error occurred.");
 			e.printStackTrace();
 		}
+	}
+
+	public int numberOfEvents() throws IOException {
+
+		int counter =1;
+
+		BufferedReader reader = new BufferedReader(new FileReader("events/events.txt"));
+
+		while (reader.readLine() != null) counter++;
+		reader.close();
+		return counter;
+
+	}
+	
+	public int getFileSize() throws IOException {
+		
+		int size = numberOfEvents()-1;
+		
+		return size;
 	}
 }
--- src/projectmain/Main.java
+++ src/projectmain/Main.java
@@ -1,19 +1,22 @@
 package projectmain;
 
+import java.io.IOException;
+
 import core.EventsHandler;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//System.out.println("MEGALUL");
 		EventsHandler eh = new EventsHandler();
-		eh.createFile();
+		//eh.createFile();
 		eh.writeToFile("hahaha");
 		eh.writeToFile("hehehe");
 		eh.writeToFile("hihihi");
 		eh.writeToFile("hohoho");
 		eh.writeToFile("huhuhu");
-		eh.readEvents();
+		eh.listEvents();
+		System.out.println(eh.getFileSize());
 	}
 
 }
