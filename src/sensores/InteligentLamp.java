1e7def67463d8af2c54c9150380320d08c9fef8b
BrunooMPS <61566542+BrunooMPS@users.noreply.github.com>
2021-12-29 15:54:25 +0000

    lamp timeout

--- src/sensores/InteligentLamp.java
+++ src/sensores/InteligentLamp.java
@@ -1,20 +1,49 @@
 package sensores;
 
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.java.proxy.BezirkMiddleware;
+
 public class InteligentLamp extends Sensor {
 	
-	private int timeOut;
+	private Bezirk bezirk;
+	long startTime;
+	
+	public InteligentLamp() {
+		super();
+		BezirkMiddleware.initialize();
+		bezirk = BezirkMiddleware.registerZirk("Inteligent Lamp");
+	}
+	
+	public void sendMovementDetectorUpdate() {
+    	//produces some  values since this is a mock
+        Boolean newState = true;
+        final String newName = "Inteligent Lamp";
+        UpdateEvent InteligentLampUpdateEvent = new UpdateEvent(newName,newState);
+        
+        //sends the event
+        bezirk.sendEvent(InteligentLampUpdateEvent);
+        startTime = System.currentTimeMillis();
+        
+        System.err.println("Published Inteligent Lamp update: " + InteligentLampUpdateEvent.toString());
+        
+        while(System.currentTimeMillis() - startTime < 10000) {
+        	continue;
+        }
+        
+        newState = false;
+        //sends new event to turn off the button after 10 seconds
+        InteligentLampUpdateEvent = new UpdateEvent(newName,newState);
+        bezirk.sendEvent(InteligentLampUpdateEvent);
+        System.out.println("Finished after 10 seconds, lamp turned OFF again");
+                
+        System.err.println("Published Inteligent Lamp update: " + InteligentLampUpdateEvent.toString());
+    }
 
-	public InteligentLamp(String sensorSame, Boolean actualState,int timeOut) {
-		super(sensorSame, actualState);
-		this.timeOut = timeOut;
+	public long getStartTime() {
+		return startTime;
 	}
 
-	public int getTimeOut() {
-		return timeOut;
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
 	}
-
-	public void setTimeOut(int timeOut) {
-		this.timeOut = timeOut;
-	}
-
 }
